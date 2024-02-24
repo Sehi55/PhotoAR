b4ae65c75d0641fd03dac3895045f89790302e95
Sehui <sehi1028@naver.com>
2024-02-25 04:04:39 +0900

    fix: custom

--- dist/custom.4596fa3d.js
+++
@@ -1,2 +0,0 @@
-const onClickMenu=t=>{const c=document.querySelectorAll(".tab");c.forEach((t=>{t.classList.remove("active")}));const e=t.target;e.classList.add("active");const o=Array.from(c).indexOf(e),s=document.querySelectorAll(".option");s.forEach((t=>{t.classList.remove("active")})),s[o].classList.add("active")};
-//# sourceMappingURL=custom.4596fa3d.js.map
--- dist/custom.b45d1c24.js
+++ dist/custom.b45d1c24.js
@@ -1,3 +1,12 @@
+const onClickRestart = (e)=>{
+    const clickedTab = e.target;
+    console.log(clickedTab);
+    const customs = document.querySelectorAll(".custom");
+    customs.forEach((custom)=>{
+        custom.classList.remove("active");
+        if (custom.getAttribute("alt") === "basic_makeup") custom.classList.add("active");
+    });
+};
 const onClickMenu = (e)=>{
     const tabs = document.querySelectorAll(".tab");
     tabs.forEach((tab)=>{
@@ -13,16 +22,30 @@ const onClickMenu = (e)=>{
     options[optionIndex].classList.add("active");
 };
 const onClickOption = (e)=>{
+    // e.target은 button이거나 img일 수 있음
+    const clickedElement = e.target; // 맨 위에 있는 img
+    let imgAlt = [];
+    let imgClassList = [];
+    // 선택한 img의 형제요소들
+    if (clickedElement.parentElement?.childElementCount) {
+        let pointChild = clickedElement.parentElement.firstElementChild;
+        for(let i = 0; i < clickedElement.parentElement?.childElementCount - 1; i++){
+            imgAlt.push(pointChild?.getAttribute("alt"));
+            imgClassList.push(pointChild?.classList[1]);
+            pointChild?.nextElementSibling ? pointChild = pointChild.nextElementSibling : pointChild = null;
+        }
+    }
     const customs = document.querySelectorAll(".custom");
     customs.forEach((custom)=>{
-        custom.classList.remove("active");
-    });
-    const clickedButton = e.target;
-    const images = clickedButton.querySelectorAll(".select");
-    // NodeList를 Array로 변환하여 forEach 사용
-    Array.from(images).forEach((img)=>{
-        const imageElement = img; // 형변환
-        console.log(imageElement.alt);
+        if (imgClassList.length > 1) {
+            custom.classList.remove("active");
+            if (imgAlt[0] === "2024_clothes" && "basic_makeup" === custom.getAttribute("alt")) custom.classList.add("active");
+        } else imgClassList.forEach((img)=>{
+            if (img === custom.classList[1]) custom.classList.remove("active");
+        });
+        imgAlt.forEach((imgAlt)=>{
+            if (imgAlt === custom.getAttribute("alt")) custom.classList.add("active");
+        });
     });
 };
 
